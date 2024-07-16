2158802717e1d3667cda8a4eff9f3023ce4e313f
Carmeshia <65563803+Meshia13@users.noreply.github.com>
2024-07-15 20:09:11 -0400

    update: updated files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,40 @@
 
-// /**
-//  * Define Global Variables
-//  * 
-// */
+/**
+ * Define Global Variables
+ * 
+*/
 
-// // Getting the <ul> element and assigning to variable
-// const myNav = document.getElementById("navBar_list");
+// Getting the <ul> element and assigning to variable
+const myNav = document.getElementById("navBar_list");
 
-// // Selecting all sections
-// const sections = document.querySelectorAll("section");
+// Selecting all sections
+const sections = document.querySelectorAll("section");
+
+/**
+ * End Global Variables
+ * 
+*/
+
+// function buildNavBar() {
+//     for (const section of sections) {
+//         const anchorEl = document.createElement("a");
+//         const links = document.createElement("li");
+//         links.classList.add("menu_link");
+//         anchorEl.href = `#${section.id}`;
+//         anchorEl.textContent = section.dataset.nav;
+
+//         anchorEl.addEventListener("click", function(clickEvent) {
+//         section.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
+//         });
+
+//         links.appendChild(anchorEl);
+//         myNav.appendChild(links);
+
+//     }
+// }
 
 
-// /**
-//  * End Global Variables
-//  * 
-// */
+// buildNavBar()
 
 // // build the nav
 
